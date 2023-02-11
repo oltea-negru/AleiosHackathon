a83db1f25615a50b63c718f4f7ffe03141d798a3
Oltea <oltea1907@gmail.com>
2023-02-11 12:27:16 +0000

    tailwind setup:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+import
+{
+  Router, Routes, Route, BrowserRouter
+} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App()
 {
   return (
-    <p>hello</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
